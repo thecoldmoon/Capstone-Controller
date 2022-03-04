47561bdb25cf6f1ce15d23bf5b79e1bececf479e
Lulu <l.l.liu.0904@gmail.com>
2022-03-03 23:07:49 -0500

    First Prototype Draft

--- Vosk/main.py
+++ Vosk/main.py
@@ -37,8 +37,8 @@ def checkMicrophone(nameString):
             return True
     return False
 
-def flashTrigger():
-    print("CALL BELL")
+def flashTrigger(reason):
+    print("CALL BELL triggered by:",reason)
     GPIO.output(triggerLight,1)
     sleep(1)
     GPIO.output(triggerLight,0)
@@ -55,15 +55,11 @@ def initiateVoice():
         print ("Please download a model for your language from https://alphacephei.com/vosk/models")
         print ("and unpack as 'model' in the current folder.")
         sys.exit()
-    device_info = sd.query_devices('default', 'input')
+    device_info = sd.query_devices(None, 'input')
     samplerate = int(device_info['default_samplerate'])
    
     model = vosk.Model(model)
-
-    with sd.RawInputStream(samplerate=samplerate, blocksize = 8000, device='default', dtype='int16',
-                            channels=1, callback=callback):
-            rec = vosk.KaldiRecognizer(model, samplerate)
-    return rec
+    return model, samplerate
 
 def checkTriggerWords(text):
     count = 0
@@ -72,50 +68,59 @@ def checkTriggerWords(text):
     return [timer() for i in range(count)]
 
 # INIT
-rec = initiateVoice() if checkMicrophone('USB Condenser Microphone') else print('Microphone not detected') and sys.exit()
+model, samplerate = initiateVoice() if checkMicrophone('USB Condenser Microphone') else print('Microphone not detected') and sys.exit()
 
 # RUN
-def main(data_dir):
+def main():
     triggerWordHistory = []
     repeats = []
+    
+    with sd.RawInputStream(samplerate=samplerate, blocksize = 8000, device=None, dtype='int16',
+                            channels=1, callback=callback):
+        rec = vosk.KaldiRecognizer(model, samplerate)
 
-    while True:
-        ## Voice Modality, Check switch is on and mic is plugged in
-        if GPIO.input(micSwitch):
-            
-            # Make sure light is on
-            GPIO.output(voiceLight,1)
+        while True:
+            ## Voice Modality, Check switch is on and mic is plugged in
+            if GPIO.input(micSwitch):
+                
+                # Make sure light is on
+                GPIO.output(voiceLight,1)
 
-            # Check for voice and trigger
-            data = q.get()
-            if rec.AcceptWaveform(data):
-                text = json.loads(rec.Result())
-                triggerWordHistory.append(checkTriggerWords(text["text"]))
-                print("RESULT", rec.Result())
-            
+                # Check for voice and trigger
+                data = q.get()
+                if rec.AcceptWaveform(data):
+                    text = json.loads(rec.Result())
+                    triggerWordHistory += checkTriggerWords(text["text"])
+                    print("RESULT", rec.Result())
+                    print("Trigger word history",triggerWordHistory)
+                    print('repeats', repeats)
+                    
+                    # Check if Threshold is met, 
+                    # Repetive, triggers if word is a single trigger word is heard consecutively
+                    if len(text["text"].split()) > 0:
+                        for word in text["text"].split():
+                            if word in TRIGGER_WORDS:
+                                if len(repeats) == 0 or repeats[-1] == word:
+                                    repeats.append(word)
+                                else:
+                                    repeats = []
+                            if len(repeats) >= 3: 
+                                flashTrigger("Voice Repeat")
+                                repeats = []
+                    # Temporal, triggers if 4 trigger words are heard consecutively within 4 seconds
+                    if len(triggerWordHistory) >= 3:
+                        if (triggerWordHistory[-1] - triggerWordHistory[0]) >= 4:
+                            flashTrigger("Voice Temporal")
+                        triggerWordHistory= []
+                
             else:
-                print("PARTIAL", rec.PartialResult())
-            
-            # Check if Threshold is met, 
-            # Repetive, triggers if word is a single trigger word is heard consecutively
-            for word in text["text"].split():
-                if word in TRIGGER_WORDS:
-                    if len(repeats) is 0 or repeats[-1] is word: repeats.append(word) 
-                if repeats >= 3: 
-                    flashTrigger()
-                    repeats = []
-            # Temporal, triggers if 4 trigger words are heard consecutively within 4 seconds
-            if len(triggerWordHistory) >= 3 and triggerWordHistory[-1] - triggerWordHistory[0] >= 4:
-                flashTrigger()
-                triggerWordHistory= []
-        else:
-            GPIO.output(voiceLight,0)
-            print("Mic Switch is Off, not listening")
+                GPIO.output(voiceLight,0)
+                print("Mic Switch is Off, not listening")
 
-        ## Call Bell Press Modality
-        if GPIO.input(callBell):
-            flashTrigger()
+            ## Call Bell Press Modality
+            if GPIO.input(callBell) == GPIO.LOW:
+                flashTrigger("Call Bell")
 
 
 if __name__ == '__main__':
-    pprint(main(sys.argv[1]))
+    pprint(main())
